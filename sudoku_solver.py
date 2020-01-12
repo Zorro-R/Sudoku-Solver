a87602f0ce1ca6eefe21b8ecac30692fda85eec3
Jurij Waite <jurijlwaite@gmail.com>
2020-01-12 14:15:53 +0100

    Finished visualization for an example puzzle. Added documentation.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,4 +1,129 @@
+import copy
+
 # Idea sparked by this post https://hackernoon.com/sudoku-and-backtracking-6613d33229af
+# Further inspiration from https://www.reddit.com/r/Python/comments/empp5x/oc_updated_version_of_my_recent_maze_finding/
+
+
+def display_board(board):
+    '''
+    Takes in a board object and displays it as a sudoku grid in the commandline.
+
+    Args:
+    board -> List[List[]]
+    Returns:
+    True
+    '''
+    for y, row in enumerate(board):
+        if y % 3 == 0:
+            print("\n---------------------")
+
+        else:
+            print()
+        for x, value in enumerate(row):
+            if x % 3 == 0:
+                print("|", end="")
+            print(value, end="|")
+    print()
+    print()
+
+    return True
+
+
+def valid_entry(board, x, y, guess):
+    '''
+    Takes a board object, a position on the board: (x, y) from the top left corner,
+    and an integer between 1-9 as a guess. Then checks the given row, column, and 
+    3x3 square for the guessed number. If no conflict is found with pre-existing 
+    entries returns True. Otherwise returns False.
+
+    Args: 
+    board -> List[List[]]
+    x, y, guess -> Int
+    Returns:
+    True if entry does not conflict with pre-existing entries 
+    else False
+    '''
+    # Calculate 3x3 subgrid
+    subgrid = [subrow[x - x % 3: x - x % 3 + 3]
+               for subrow in board[y - y % 3: y - y % 3 + 3]]
+
+    # Check row and column
+    row = board[y]
+    column = [row[x] for row in board]
+    if guess in row or guess in column:
+        return False
+
+    # Check subgrid
+    for row in subgrid:
+        if guess in row:
+            return False
+
+    # If guess does not conflict with pre-existing entries return True
+    return True
+
+
+def full_board(board):
+    '''
+    Checks if all squares on the board have been filled. 
+    Returns True if this is the case.
+
+    Args:
+    board -> List[List[]]
+    Returns:
+    True if no cell is 0 (i.e empty)
+    else False
+    '''
+    for row in board:
+        for value in row:
+            if value == 0:
+                return False
+
+    return True
+
+
+def recursive_backtracking(board, list_of_boards=[], save_boards=True):
+    '''
+    Function that applies recursive backtracking to a board object.
+    Returns True if the board could be solved and False if no solution 
+    could be found. Modifies the board object itself. Also appends each
+    board configuration to a list_of_boards (Can be disabled by setting
+    save_boards to False).
+
+    Args:
+    board -> List[List[]]
+    list_of_boards -> List[]
+    save_boards -> Bool
+    Returns:
+    True if board can be filled given constraints
+    else False
+    '''
+    # Base case - board is full
+    if full_board(board):
+        return True
+
+    # Recursive case - board isn't full
+    for y, row in enumerate(board):
+        for x, value in enumerate(row):
+            if value == 0:  # If cell is empty
+                for guess in range(1, 10):
+                    # If guess does not cause a conflict, save the guess to the board
+                    if valid_entry(board, x, y, guess):
+                        board[y][x] = guess
+
+                        # If save_boards is True append copy of board to list_of_boards
+                        if save_boards:
+                            list_of_boards.append(copy.deepcopy(board))
+                        # If this candidate guess works return True
+                        if recursive_backtracking(board, list_of_boards, save_boards):
+                            return True
+                        # Otherwise reset the cell and try the next guess
+                        else:
+                            board[y][x] = 0
+
+                # In the case that no guess works, the board cannot be solved so we return False
+                return False
+
+
 # Sudoku puzzle taken from https://www.websudoku.com/?level=1&set_id=1559770422
 board1 = [
     [5, 0, 0, 6, 0, 0, 0, 0, 0],
@@ -26,104 +151,26 @@ board2 = [
 ]
 
 
-def display_board(board):
+def main():
     '''
-    Takes in a board object and displays it as a sudoku grid in the commandline.
+    Runs the recursive backtracking algorithm on two example puzzles.
     '''
-    for y, row in enumerate(board):
-        if y % 3 == 0:
-            print("\n---------------------")
+    # Run backtracking algorithm on board 1
+    print("Example 1")
+    display_board(board1)
+    list_of_boards1 = [copy.deepcopy(board1)]
+    recursive_backtracking(board1, list_of_boards1)
+    print("Example 1 Solution")
+    display_board(board1)
 
-        else:
-            print()
-        for x, value in enumerate(row):
-            if x % 3 == 0:
-                print("|", end="")
-            print(value, end="|")
-    print()
-    print()
-
-    return True
-
-
-def valid_entry(board, x, y, guess):
-    '''
-    Takes a board object, a position on the board: (x, y) from the top left corner,
-    and an integer between 1-9 as a guess. Then checks the given row, column, and 
-    3x3 square for the guessed number. If no conflict is found with pre-existing 
-    entries returns True. Otherwise returns False.
-    '''
-    # Calculate 3x3 subgrid
-    subgrid = [subrow[x - x % 3: x - x % 3 + 3]
-               for subrow in board[y - y % 3: y - y % 3 + 3]]
-
-    # Check row and column
-    row = board[y]
-    column = [row[x] for row in board]
-    if guess in row or guess in column:
-        return False
-
-    # Check subgrid
-    for row in subgrid:
-        if guess in row:
-            return False
-
-    # If guess does not conflict with pre-existing entries return True
-    return True
+    # Run backtracking algorithm on board 2
+    print("Example 2")
+    display_board(board2)
+    list_of_boards2 = [copy.deepcopy(board2)]
+    recursive_backtracking(board2, list_of_boards2)
+    print("Example 2 Solution")
+    display_board(board2)
 
 
-def full_board(board):
-    '''
-    Checks if all squares on the board have been filled. 
-    Returns True if this is the case.
-    '''
-    for row in board:
-        for value in row:
-            if value == 0:
-                return False
-
-    return True
-
-
-def recursive_backtracking(board):
-    '''
-    Function that applies recursive backtracking to a board object.
-    Returns True if the board could be solved and False if no solution 
-    could be found. Modifies the board object itself.
-    '''
-    # Base case - board is full
-    if full_board(board):
-        return True
-
-    # Recursive case - board isn't full
-    for y, row in enumerate(board):
-        for x, value in enumerate(row):
-            if value == 0:  # If cell is empty
-                for guess in range(1, 10):
-                    # If guess does not cause a conflict, save the guess to the board
-                    if valid_entry(board, x, y, guess):
-                        board[y][x] = guess
-                        # If this candidate guess works return True
-                        if recursive_backtracking(board):
-                            return True
-                        # Otherwise reset the cell and try the next guess
-                        else:
-                            board[y][x] = 0
-
-                # In the case that no guess works, the board cannot be solved so we return False
-                return False
-
-
-# Run backtracking algorithm on board 1
-print("Example 1")
-display_board(board1)
-recursive_backtracking(board1)
-print("Example 1 Solution")
-display_board(board1)
-
-# Run backtracking algorithm on board 2
-print("Example 2")
-display_board(board2)
-recursive_backtracking(board2)
-print("Example 2 Solution")
-display_board(board2)
+if __name__ == "__main__":
+    main()
--- sudoku_solver_visualization.py
+++ sudoku_solver_visualization.py
@@ -1,52 +1,115 @@
 import sys
 import pygame
+import copy
+import time
+from sudoku_solver import valid_entry, full_board, recursive_backtracking
 
 # Constants
 size = width, height = 800, 800
 white = 255, 255, 255
 black = 0, 0, 0
+red = 255, 0, 0
+green = 0, 255, 0
+blue = 0, 0, 255
+# Example sudoku puzzle from https://www.websudoku.com/?level=4&set_id=1044837704
+board = [
+    [5, 0, 0, 6, 0, 0, 0, 0, 0],
+    [0, 0, 0, 8, 5, 2, 9, 0, 1],
+    [1, 6, 0, 0, 0, 0, 2, 5, 8],
+    [9, 0, 0, 0, 0, 8, 4, 0, 3],
+    [0, 0, 0, 9, 3, 7, 0, 0, 0],
+    [6, 0, 8, 5, 0, 0, 0, 0, 9],
+    [7, 2, 5, 0, 0, 0, 0, 9, 4],
+    [8, 0, 3, 4, 7, 5, 0, 0, 0],
+    [0, 0, 0, 0, 0, 9, 0, 0, 5]
+]
+
+
+def draw_board_outline(screen):
+    '''
+    Draws the outline of the sudoku board onto a pygame
+    screen object
+
+    Args:
+    screen -> pygame.Surface
+    Returns:
+    None
+    '''
+    # Draw white background
+    screen.fill(white)
+    # Draw the board
+    for i in range(10):
+        # Indicate 3x3 areas on the board with thicker outlines
+        if i % 3 == 0:
+            line_width = 3
+        else:
+            line_width = 1
+
+        # Draw vertical lines
+        start_pos_vertical = 102 + i * 65, 102
+        end_pos_vertical = 102 + i * 65, 686
+        pygame.draw.line(
+            screen, black, start_pos_vertical, end_pos_vertical, line_width)
+
+        # Draw horizontal lines
+        start_pos_horizontal = 102, 102 + i * 65
+        end_pos_horizontal = 686, 102 + i * 65
+        pygame.draw.line(
+            screen, black, start_pos_horizontal, end_pos_horizontal, line_width)
 
 
 def main():
+    '''
+    Visualizes the recursive backtracking algorithm on a sample puzzle
+    using pygame.
+    '''
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Sudoku Solver Visualization")
 
-    font = pygame.font.SysFont("Times New Roman", 30)
+    font = pygame.font.SysFont("Times New Roman", 45)
+
+    # A list that will track the progress of the algorithm
+    list_of_boards = [copy.deepcopy(board)]
+    # Run the backtracking algorithm
+    recursive_backtracking(board, list_of_boards)
+
+    isSolved = False
+    isRunning = True
 
     # Game loop
-    while True:
+    while isRunning:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
+                isRunning = False
 
-        # Draw white background
-        screen.fill(white)
-        # Draw the board
-        for i in range(10):
-            # Indicate 3x3 areas on the board with thicker outlines
-            if i % 3 == 0:
-                line_width = 3
-            else:
-                line_width = 1
+        if not isSolved:
+            # Display each board configuration in order
+            for i, board_configuration in enumerate(list_of_boards):
+                # Draw the board outline
+                draw_board_outline(screen)
 
-            # Draw vertical lines
-            start_pos_vertical = 102 + i * 65, 102
-            end_pos_vertical = 102 + i * 65, 686
-            pygame.draw.line(
-                screen, black, start_pos_vertical, end_pos_vertical, line_width)
+                # Overlay the board values
+                for y, row in enumerate(board_configuration):
+                    for x, value in enumerate(row):
+                        # If cell is not empty (i.e 0) display value
+                        if value != 0:
+                            textsurface = font.render(str(value), True, black)
+                            screen.blit(
+                                textsurface, (102 + 20 + x*65, 102 + 7 + y*65))
 
-            # Draw horizontal lines
-            start_pos_horizontal = 102, 102 + i * 65
-            end_pos_horizontal = 686, 102 + i * 65
-            pygame.draw.line(
-                screen, black, start_pos_horizontal, end_pos_horizontal, line_width)
+                # Update the screen
+                pygame.display.update()
 
-        # Overlay the board values
-        textsurface = font.render("Test", True, black)
+                # Add a delay
+                # FIX: Causes the screen to freeze while algorithm is running
+                pygame.time.delay(30)
 
-        # Update the screen
-        screen.blit(textsurface, (0, 0))
-        pygame.display.update()
+            if i == len(list_of_boards) - 1:
+                isSolved = True
+
+    pygame.display.quit()
+    pygame.quit()
+    sys.exit()
 
 
 if __name__ == '__main__':
